5a1db4851a5fe8b808f9a6897fd77ba814f36a41
SUPERGAMESGROUP <SUPERGAMESGROUP@protonmail.com>
2019-07-07 20:06:26 +0300

    +MouseLook +Spin

--- SpaceShooter/Assets/Scripts/Spin.cs
+++ SpaceShooter/Assets/Scripts/Spin.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Spin : MonoBehaviour
 {
+    public float speed = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,13 @@ public class Spin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (speed == 6.0f)
+        { //если скорость равна 6
+            transform.Rotate(0, speed, 0);
+        }
+        else
+        { //иначе куб поворачивается в другую сторону
+            transform.Rotate(0, 0, 0);
+        }
     }
 }
