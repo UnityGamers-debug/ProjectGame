0f3783096d0b00648620a184f202e1f3cca3695b
SUPERGAMESGROUP <SUPERGAMESGROUP@protonmail.com>
2019-07-13 23:42:22 +0300

    +Shoot +BigShuttle

--- SpaceShooter/Assets/Scripts/Spin.cs
+++ SpaceShooter/Assets/Scripts/Spin.cs
@@ -4,14 +4,8 @@ using UnityEngine;
 
 public class Spin : MonoBehaviour
 {
-    public float speed = 3.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 3.0f; //Объявление общедоступной переменной для скорости вращения
 
-    // Update is called once per frame
     void Update()
     {
         if (speed == 6.0f)
@@ -20,7 +14,7 @@ public class Spin : MonoBehaviour
         }
         else
         { //иначе куб поворачивается в другую сторону
-            transform.Rotate(0, 0, 0);
+            transform.Rotate(0, -speed, 0);
         }
     }
 }
