d33168a40723f11455bcf1cb22901daa848bf740
SUPERGAMESGROUP <SUPERGAMESGROUP@protonmail.com>
2019-07-20 22:14:42 +0300

    +Menu

--- SpaceShooter/Assets/Scripts/AIEnemy.cs
+++ SpaceShooter/Assets/Scripts/AIEnemy.cs
@@ -8,26 +8,40 @@ public class AIEnemy : MonoBehaviour
     public int damage = 1; //урон, наносимый персонажу
     public float maxDistance = 10.0f; //максимальная дистанция обнаружения героя
     public float minDistance = 1.0f;
+    private bool _alive;//логическая переменная для слежения
 
     public float distance; //переменная для опр – я дистанции между игроком и врагом
     public Transform target; //объект, за которым будет следить враг
     public PlayerCharacter player;
 
+    void Start()
+    {
+        _alive = true; //делаем ее в начале истинной
+    }
+
     void Update()
     {
-        distance = Vector3.Distance(transform.position, target.position); //определяем дистанцию
-        if (distance > maxDistance)
+        if (_alive)
         {
-            transform.Translate(0, 0, speed * Time.deltaTime);//непрерывно движемся вперед в каждом кадре
+            distance = Vector3.Distance(transform.position, target.position); //определяем дистанцию
+            if (distance > maxDistance)
+            {
+                transform.Translate(0, 0, speed * Time.deltaTime);//непрерывно движемся вперед в каждом кадре
+            }
+            if (distance < maxDistance && distance > minDistance)
+            {
+                transform.rotation = Quaternion.Slerp(transform.localRotation, Quaternion.LookRotation(target.position - transform.position), speed * Time.deltaTime);
+                transform.position = Vector3.Lerp(transform.position, target.position, speed * Time.deltaTime);
+            }
+            if (distance < minDistance)
+            {
+                player.Hurt(damage);
+            }
         }
-        if (distance < maxDistance && distance > minDistance)
-        {
-            transform.rotation = Quaternion.Slerp(transform.localRotation, Quaternion.LookRotation(target.position - transform.position), speed * Time.deltaTime);
-            transform.position = Vector3.Lerp(transform.position, target.position, speed * Time.deltaTime);
-        }
-        if (distance < minDistance)
-        {
-            player.Hurt(damage);
-        }
+    }
+
+    public void SetAlive(bool alive)
+    {//открывает метод,который из вне позволяет воздействовать на переменную
+        _alive = alive;
     }
 }
---
+++ SpaceShooter/Assets/Scripts/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string itemName; //Вводим имена в во вкладку Inspector
+    private int _score; // счетчик очков
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Item collected: " + itemName); // вывод взятого объекта в консоль
+        Destroy(this.gameObject);
+        if (itemName == "cube")
+        {//проверяем, является ли объект здоровьем
+            _score++; //увеличиваем счетчик очков
+            Debug.Log("Score: " + _score);
+        }
+    }
+}
